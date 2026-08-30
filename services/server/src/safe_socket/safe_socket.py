5964a42858c1823961ccbc07c6ba558b51a4d88a
FrancoBossi <fbossi@fi.uba.ar>
2026-08-30 12:19:30 -0300

    implemento funcion  en server para enviar los bytes.

--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -7,5 +7,11 @@ def recv_all(socket: socket.socket, size):
     return socket.recv(size)
 
 
-def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+def send_all(sock: socket.socket, data):
+    total_sent = 0  #bytes enviados
+    while total_sent < len(data):
+        sent = sock.send(data[total_sent:])
+        if sent == 0:
+            raise ConnectionError(f"No se envio informacion: enviados {total_sent}/{len(data)} bytes")
+        total_sent += sent
+    return total_sent
