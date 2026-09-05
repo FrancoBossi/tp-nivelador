63313b379053b088ee25ba76405bdafea78f530a
FrancoBossi <fbossi@fi.uba.ar>
2026-09-05 15:42:39 -0300

    formato original de test

--- tests/utils/agency_file_generator.py
+++ tests/utils/agency_file_generator.py
@@ -1,5 +1,8 @@
-def generate(filepath, bet_amount):
-    with open(filepath, "w") as agency_file:
-        for i in range(bet_amount):
-            agency_file.write(f"A,B,{str(i).zfill(8)},2000-01-01,{i}\n")
-        agency_file.flush()
+def generate(filepath, register_amount):
+    lines = []
+
+    for i in range(register_amount):
+        lines.append(f"A,B,{str(i).zfill(8)},2000-01-01,{i}\n")
+
+    with open(filepath, "w") as file:
+        file.write("".join(lines))
